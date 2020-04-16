9a924da63d1481f44d22549369fc10ae81c00e0e
Sunny Park <dlstjs8246@naver.com>
2020-04-16 09:43:29 +0900

    메서드 추출, 메서드 이동, 임시변수를 메서드 호출로 전환 리팩토링 적용

--- src/main/java/refactoring_concept/Customer.java
+++ src/main/java/refactoring_concept/Customer.java
@@ -12,6 +12,8 @@ public class Customer {
 	public void addRental(Rental rental) {
 		this.rentals.add(rental);
 	}
+	
+	
 	public String getName() {
 		return name;
 	}
@@ -21,34 +23,16 @@ public class Customer {
 		
 		StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 		for(Rental each : rentals) {
-			double thisAmount = amountFor(each);
 			frequentRenterPoints++;
 			if((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) frequentRenterPoints++;
 			result.append("\t" + each.getMovie().getTitle());
-			result.append("\t" + String.valueOf(thisAmount) + "\n");
+			result.append("\t" + String.valueOf(each.getCharge()) + "\n");
 			
-			totalAmount += thisAmount;
+			totalAmount += each.getCharge();
 		}
 		result.append("누적 대여료 : " + String.valueOf(totalAmount) + "\n");
 		result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
 		
 		return result.toString();
-	}
-	private double amountFor(Rental aRental)  {
-		double result = 0;
-		switch(aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if(aRental.getDaysRented() > 2) result += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDENS:
-			result += 1.5;
-			if(aRental.getDaysRented()>3) result += (aRental.getDaysRented() -3) * 1.5;
-			break;
-		}
-		return result;
 	}
 }
